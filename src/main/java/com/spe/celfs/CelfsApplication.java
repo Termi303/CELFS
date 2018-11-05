152cc89227245fe9c66e01a507ce733c04da1f52
Igor <id17002@my.bristol.ac.uk>
2018-11-05 12:10:57 +0000

    Maven/Spring Template

---
+++ src/main/java/com/spe/celfs/CelfsApplication.java
@@ -0,0 +1,12 @@
+package com.spe.celfs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CelfsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CelfsApplication.class, args);
+	}
+}
