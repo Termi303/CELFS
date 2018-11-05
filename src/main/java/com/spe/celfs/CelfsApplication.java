5ba3812b9919bf2ede7e75cce6b5dac616817ecb
Igor <id17002@my.bristol.ac.uk>
2018-11-05 12:33:05 +0000

    Proper shutdown

--- src/main/java/com/spe/celfs/CelfsApplication.java
+++ src/main/java/com/spe/celfs/CelfsApplication.java
@@ -2,11 +2,14 @@ package com.spe.celfs;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class CelfsApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(CelfsApplication.class, args);
+		ConfigurableApplicationContext app = SpringApplication.run(CelfsApplication.class, args);
+                ((ConfigurableApplicationContext) app).close();
 	}
 }
