6442bf0c5f3514004623ecf25c7ced8ec91ee0cb
Todor <todormilenovhristov@gmail.com>
2018-11-13 18:38:55 +0000

    Remove Santa Grotto

--- src/main/java/ac/uk/bristol/cs/santa/grotto/business/data/EventRepository.java
+++
@@ -1,9 +0,0 @@
-package ac.uk.bristol.cs.santa.grotto.business.data;
-
-import org.springframework.data.repository.CrudRepository;
-
-/**
- * Created by csxds on 26/11/2017.
- */
-public interface EventRepository extends CrudRepository<Event, Long> {
-}
