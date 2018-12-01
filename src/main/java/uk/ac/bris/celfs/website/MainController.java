619a429f8163b690b8490e4de2721ab237d869a7
Todor <todormilenovhristov@gmail.com>
2018-12-01 21:46:07 +0000

    Added fields to MainController for MrrRaw

--- src/main/java/uk/ac/bris/celfs/website/MainController.java
+++ src/main/java/uk/ac/bris/celfs/website/MainController.java
@@ -7,6 +7,7 @@ import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
@@ -55,11 +56,54 @@ public class MainController {
     }
 
     @PostMapping(value = "/mrr")
-    public String submitMrr(@Valid MrrRaw mrrRaw, BindingResult binding, RedirectAttributes attr) {
+    public String submitMrr(@RequestParam("studentID")                  String studentID,
+                            @RequestParam("response_comment")           String response_comment,
+                            @RequestParam("response")                   String response,
+                            @RequestParam("method_comment")             String method_comment,
+                            @RequestParam("method")                     String method,
+                            @RequestParam("results_comment")            String results_comment,
+                            @RequestParam("results")                    String results,
+                            @RequestParam("discussion_comment")         String discussion_comment,
+                            @RequestParam("discussion")                 String discussion,
+                            @RequestParam("synthesis_comment")          String synthesis_comment,
+                            @RequestParam("synthesis")                  String synthesis,
+                            @RequestParam("reasoning_comment")          String reasoning_comment,
+                            @RequestParam("reasoning")                  String reasoning,
+                            @RequestParam("control_comment")            String control_comment,
+                            @RequestParam("control")                    String control,
+                            @RequestParam("errors_comment")             String errors_comment,
+                            @RequestParam("errors")                     String errors,
+                            @RequestParam("nounphrases_comment")        String nounphrases_comment,
+                            @RequestParam("nounphrases")                String nounphrases,
+                            @RequestParam("vocabulary_comment")         String vocabulary_comment,
+                            @RequestParam("vocabulary")                 String vocabulary,
+                            @RequestParam("wordchoice_comment")         String wordchoice_comment,
+                            @RequestParam("wordchoice")                 String wordchoice,
+                            @RequestParam("style_comment")              String style_comment,
+                            @RequestParam("style")                      String style,
+                            @RequestParam("sentencestructure_comment")  String sentencestructure_comment,
+                            @RequestParam("sentencestructure")          String sentencestructure,
+                            @RequestParam("organisation_comment")       String organisation_comment,
+                            @RequestParam("organisation")               String organisation,
+                            @RequestParam("development_comment")        String development_comment,
+                            @RequestParam("development")                String development,
+                            @RequestParam("cohesivedevices_comment")    String cohesivedevices_comment,
+                            @RequestParam("cohesivedevices")            String cohesivedevices,
+                            @RequestParam("conclusion_comment")         String conclusion_comment,
+                            @RequestParam("conclusion")                 String conclusion,
+                            @RequestParam("presentation_comment")       String presentation_comment,
+                            @RequestParam("presentation")               String presentation,
+                            @RequestParam("overallComment")             String overallComment,
+                            BindingResult binding,
+                            RedirectAttributes attr) {
         if (binding.hasErrors()) {
             return "/error";
         }
-        mrrRawRepository.save(mrrRaw);
+        MrrRaw myMrrRaw = new MrrRaw();
+        myMrrRaw.studentID = studentID;
+        myMrrRaw.response_comment = response_comment;
+        myMrrRaw.response = response;
+        mrrRawRepository.save(myMrrRaw);
         // attr.addFlashAttribute("message", "Thank you for your quote.");
         return "redirect:/reviewmrr";
     }
