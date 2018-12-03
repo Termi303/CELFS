5c065fc9318a5a8f613b89c11db23d4b7df31333
taletreader <tx17079@my.bristol.ac.uk>
2018-12-03 17:30:16 +0000

    added a comment

--- src/main/java/uk/ac/bris/celfs/website/RoundMarks.java
+++ src/main/java/uk/ac/bris/celfs/website/RoundMarks.java
@@ -1,7 +1,7 @@
 import java.util.Arrays;
 import java.util.*;
 
-
+//Based on code from: https://stackoverflow.com/questions/8612891/how-can-i-round-down-to-the-nearest-integer-from-an-int-array
 public class RoundMarks {
     private static int[]  marks= {0,7,15,22,29,36,42,45,48,52,55,58,62,65,68,72,75,78,83,89,94};
 
