fe241a69346f03b0ff236f81c2f0a5236b690ef8
taletreader <tx17079@my.bristol.ac.uk>
2018-12-03 16:55:49 +0000

    fixed list, is now correct

--- src/main/java/uk/ac/bris/celfs/website/RoundMarks.java
+++ src/main/java/uk/ac/bris/celfs/website/RoundMarks.java
@@ -3,7 +3,7 @@ import java.util.*;
 
 
 public class RoundMarks {
-    private static int[]  marks= {0,5,10};
+    private static int[]  marks= {0,7,15,22,29,36,42,45,48,52,55,58,62,65,68,72,75,78,83,89,94};
 
     private static int applyMark(int x) {
         int applied = 0;
