178049f6b9ecf0d8bded8ca239765ff80bc70ddf
Lucas Dale <ld17852@bristol.ac.uk>
2018-12-05 11:21:04 +0000

    tidied

--- src/main/java/uk/ac/bris/celfs/website/MrrCommand.java
+++ src/main/java/uk/ac/bris/celfs/website/MrrCommand.java
@@ -10,110 +10,74 @@ public class MrrCommand {
     
     
     String response_comment;
-    
-    
     String response;
     
     
-    String method_comment;
-    
-    
+    String method_comment; 
     String method;
     
     
     String results_comment;
-    
-    
     String results;
     
     
     String discussion_comment;
-    
-    
     String discussion;
     
     
     String synthesis_comment;
-    
-    
     String synthesis;
     
     
     String reasoning_comment;
-    
-    
     String reasoning;
     
     
-    String control_comment;
-    
-    
+    String control_comment; 
     String control;
     
     
     String errors_comment;
-    
-    
     String errors;
     
     
     String nounphrases_comment;
-    
-    
     String nounphrases;
     
     
     String vocabulary_comment;
-    
-    
     String vocabulary;
     
     
     String wordchoice_comment;
-    
-    
     String wordchoice;
     
     
     String style_comment;
-    
-    
     String style;
     
     
     String sentencestructure_comment;
-    
-    
     String sentencestructure;
     
     
     String organisation_comment;
-    
-    
     String organisation;
     
     
     String development_comment;
-    
-    
     String development;
     
     
     String cohesivedevices_comment;
-    
-    
     String cohesivedevices;
     
     
     String conclusion_comment;
-    
-    
     String conclusion;
     
     
     String presentation_comment;
-    
-    
     String presentation;
     
     
