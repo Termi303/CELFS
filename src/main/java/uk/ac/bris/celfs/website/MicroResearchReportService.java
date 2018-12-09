3ffc041347bee05285c7094b3e1b47ed0a8c74cc
Igor <id17002@my.bristol.ac.uk>
2018-12-09 21:47:58 +0000

    Coding in Github editor is not a good idea

--- src/main/java/uk/ac/bris/celfs/website/MicroResearchReportService.java
+++ src/main/java/uk/ac/bris/celfs/website/MicroResearchReportService.java
@@ -1,16 +1,28 @@
 package uk.ac.bris.celfs.website;
 
 import lombok.Data;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import javax.persistence.*;
 import javax.validation.constraints.NotEmpty;
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class MicroResearchReportService {
-    @Autowired MicroResearchReportRepository reportRepository;
+    @Autowired
+    MicroResearchReportRepository reportRepository;
     
     public void add(MicroResearchReport report) {
         reportRepository.save(report);
+    }
+
+    public List<MicroResearchReport> getAll() {
+        List<MicroResearchReport> reports = new ArrayList<>();
+        reportRepository.findAll()
+                .forEach(reports::add);
+        return reports;
     }
     
 }
