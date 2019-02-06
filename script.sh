9d71c8aa72b5ac3963636475d612f95d40baa7ed
Todor <todormilenovhristov@gmail.com>
2019-02-06 23:35:02 +0000

    Final version

--- script.sh
+++ script.sh
@@ -3,16 +3,7 @@ myhappyscript()
   sudo kill $(sudo lsof -t -i:8080)
 }
 
-somescript()
-{
-  nohup java -jar ~/JARFILE/website-0.0.1-SNAPSHOT.jar &
-}
-
 set -e
 echo ein
 myhappyscript || true
 echo zwei
-sudo iptables -F
-echo drei
-somescript &
-echo vier
