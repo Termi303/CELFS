a1272d7894412d4cb17a41597095477517af25c2
Todor <todormilenovhristov@gmail.com>
2019-03-05 16:35:11 +0000

    Fix CD Script to remove all programs on port 80

--- script.sh
+++ script.sh
@@ -1,7 +1,13 @@
-myhappyscript()
+myhappyscript1()
 {
   sudo kill $(sudo lsof -t -i:8080)
 }
 
+myhappyscript2()
+{
+  sudo kill $(sudo lsof -t -i:80)
+}
+
 set -e
-myhappyscript || true
+myhappyscript1 || true
+myhappyscript2 || true
