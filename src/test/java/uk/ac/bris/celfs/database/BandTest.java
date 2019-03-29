9478cb671feafc164b4ece1813f733319d80474a
Unknown <termi303@gmail.com>
2019-03-29 11:57:11 +0000

    BandTest - need fixing

--- src/test/java/uk/ac/bris/celfs/database/BandTest.java
+++ src/test/java/uk/ac/bris/celfs/database/BandTest.java
@@ -10,10 +10,14 @@ import uk.ac.bris.celfs.coursework.CourseworkRepository;
 import static org.junit.Assert.assertEquals;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
+import javax.annotation.Resource;
+import java.util.ArrayList;
+import java.util.List;
+
 @RunWith(SpringRunner.class)
 @DataJpaTest
 public class BandTest {
-    @Autowired
+    @Resource
     private BandRepository bandRepository;
 
     @Test
@@ -24,5 +28,56 @@ public class BandTest {
 
         assert(band.getId() != null);
         assertEquals(name, band.getName());
+    }
+
+    @Test
+    public void testCreateOneBandWithDescription() {
+        String name = "Exceptional";
+        String description = "Sample description";
+        Band band = new Band(name);
+        band.setDescription(description);
+        bandRepository.save(band);
+
+        assertEquals(description, band.getDescription());
+    }
+
+    private int getIndex(String name) {
+        int result = 0;
+        int multiplier = 1;
+        int where = name.length()-1;
+        while(name.charAt(where) == '_') {
+            result += multiplier * (name.charAt(where--) - '0');
+            multiplier *= 10;
+        }
+        return result;
+    }
+
+    @Test
+    public void testCreateMultipleBands() {
+        int howMany = 100;
+        String default_name = "BAND_NAME_";
+        String default_description = "BAND_DESCRIPTION_";
+        for(int i = 0; i < howMany; i++) {
+            String name = default_name + i;
+            String description = default_description + i;
+            Band band = new Band(name);
+            band.setDescription(description);
+            bandRepository.save(band);
+        }
+        List<Band> allBands = new ArrayList<>();
+        bandRepository.findAll()
+                .forEach(allBands::add);
+
+        assertEquals(howMany, allBands.size());
+        boolean[] seenBands = new boolean[howMany];
+        for(Band band : allBands) {
+            String name = band.getName();
+            String description = band.getDescription();
+            int i = getIndex(name);
+            assertEquals((default_name + i), name);
+            assertEquals((default_description + i), description);
+            assertEquals(false, seenBands[i]);
+            seenBands[i] = true;
+        }
     }
 }
