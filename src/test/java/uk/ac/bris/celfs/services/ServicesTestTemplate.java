bcfb04143abac45b3f147864cb1d99b9090035c5
Unknown <termi303@gmail.com>
2019-04-15 16:46:14 +0200

    Add tests in KeywordServiceTest, NEEDS BEAN FIX

--- src/test/java/uk/ac/bris/celfs/services/ServicesTestTemplate.java
+++ src/test/java/uk/ac/bris/celfs/services/ServicesTestTemplate.java
@@ -1,6 +1,9 @@
 package uk.ac.bris.celfs.services;
 
 
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.junit4.SpringRunner;
 import uk.ac.bris.celfs.coursework.CourseworkTestTemplate;
 
 import javax.annotation.Resource;
