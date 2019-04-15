901dfa984de2a93b0c3fa213590e44bfaad56820
Todor <todormilenovhristov@gmail.com>
2019-04-15 18:14:39 +0100

    Added port 443 to script.sh

--- script.sh
+++ script.sh
@@ -8,6 +8,12 @@ myhappyscript2()
   sudo kill $(sudo lsof -t -i:80)
 }
 
+myhappyscript3()
+{
+  sudo kill $(sudo lsof -t -i:443)
+}
+
 set -e
 myhappyscript1 || true
 myhappyscript2 || true
+myhappyscript3 || true
