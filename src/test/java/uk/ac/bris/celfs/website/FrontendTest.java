bf80e073747f79443a42f5a926a01ee32e4e3686
Equinurmae <lucasdale4444@gmail.com>
2019-04-24 13:58:53 +0100

    selenium tests for login

--- src/test/java/uk/ac/bris/celfs/website/FrontendTest.java
+++ src/test/java/uk/ac/bris/celfs/website/FrontendTest.java
@@ -9,19 +9,28 @@ import static org.junit.Assert.assertEquals;
 public class FrontendTest {
     public static void main(String[] args){
         FirefoxDriver driver=new FirefoxDriver();
-        driver.get("https://celfs.spe.cs.bris.ac.uk/login");
         WebElement username=driver.findElement(By.xpath("//input[@name='username']"));
-        username.sendKeys("bad_teacher");
         WebElement button=driver.findElement(By.tagName("button"));
+        WebElement password=driver.findElement(By.xpath("//input[@name='password']"));
+
+        //test wrong email and no password
+        driver.get("https://celfs.spe.cs.bris.ac.uk/login");
+        username.sendKeys("bad_teacher");
         button.click();
-//        System.out.println("The URL is " + driver.getCurrentUrl());
         assertEquals((Object) "https://celfs.spe.cs.bris.ac.uk/login", (Object) driver.getCurrentUrl());
 
+        //test correct email/password
         driver.get("https://celfs.spe.cs.bris.ac.uk/login");
-        WebElement password=driver.findElement(By.xpath("//input[@name='password']"));
         username.sendKeys("teacher1");
         password.sendKeys("teacher");
         button.click();
         assertEquals((Object) "https://celfs.spe.cs.bris.ac.uk", (Object) driver.getCurrentUrl());
+
+        //test correct email and incorrect password
+        driver.get("https://celfs.spe.cs.bris.ac.uk/login");
+        username.sendKeys("teacher1");
+        password.sendKeys("bad_password");
+        button.click();
+        assertEquals((Object) "https://celfs.spe.cs.bris.ac.uk/login", (Object) driver.getCurrentUrl());
     }
 }
